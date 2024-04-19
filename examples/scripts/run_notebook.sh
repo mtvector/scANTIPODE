698024d77832902cb5630746cb89082f25b42d46
Matthew Schmitz <matthew.schmitz@hpc-login.corp.alleninstitute.org>
2024-04-18 21:09:24 -0700

    more broken

--- examples/scripts/run_notebook.sh
+++ examples/scripts/run_notebook.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name singnbconv
 #SBATCH --output /home/matthew.schmitz/log/nbconvert_%A_%a.out # %A is replaced by job ID, %a by array index
 #SBATCH --error /home/matthew.schmitz/log/nbconvert_%A_%a.err
-#SBATCH --time 72:00:00
+#SBATCH --time 70:00:00
 #SBATCH --partition celltypes
 #SBATCH --gres=gpu:1 --constraint="a100|v100"#
-#SBATCH --mem 256gb
+#SBATCH --mem 128gb
 #SBATCH --ntasks 1
 
 source ~/.bashrc
@@ -15,7 +15,7 @@ source ~/.bashrc
 conda activate antipode
 
 # Read the notebook path for the current array task
-NOTEBOOK=/allen/programs/celltypes/workgroups/rnaseqanalysis/EvoGen/Team/Matthew/code/scANTIPODE/examples/1.9.1.8.3_RunJorstadAllNoPsi.ipynb
+NOTEBOOK=/allen/programs/celltypes/workgroups/rnaseqanalysis/EvoGen/Team/Matthew/code/scANTIPODE/examples/1.9.1.8.1.3_OriginalFest.ipynb
 
 # Command to run jupyter nbconvert
 jupyter nbconvert --ExecutePreprocessor.allow_errors=True --to html --execute "${NOTEBOOK}" --output ~/Matthew/code/scANTIPODE/examples/outputs/"executed_$(basename "${NOTEBOOK}")" 
