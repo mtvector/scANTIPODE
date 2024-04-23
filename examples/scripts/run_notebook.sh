d59ae2a16e33c22770ad8850a019f828fefcee0a
Matthew Schmitz <matthew.schmitz@hpc-login.corp.alleninstitute.org>
2024-04-23 12:14:37 -0700

    settle with pseudoz for stability

--- examples/scripts/run_notebook.sh
+++ examples/scripts/run_notebook.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name singnbconv
 #SBATCH --output /home/matthew.schmitz/log/nbconvert_%A_%a.out # %A is replaced by job ID, %a by array index
 #SBATCH --error /home/matthew.schmitz/log/nbconvert_%A_%a.err
-#SBATCH --time 70:00:00
+#SBATCH --time 30:00:00
 #SBATCH --partition celltypes
 #SBATCH --gres=gpu:1 --constraint="a100|v100"#
-#SBATCH --mem 128gb
+#SBATCH --mem 255gb
 #SBATCH --ntasks 1
 
 source ~/.bashrc
@@ -15,7 +15,8 @@ source ~/.bashrc
 conda activate antipode
 
 # Read the notebook path for the current array task
-NOTEBOOK=/allen/programs/celltypes/workgroups/rnaseqanalysis/EvoGen/Team/Matthew/code/scANTIPODE/examples/1.9.1.8.3_Relu-NoInterceptOnApprox-Longer-ResetDC-NotLeaky.ipynb
+NOTEBOOK=/allen/programs/celltypes/workgroups/rnaseqanalysis/EvoGen/Team/Matthew/code/scANTIPODE/examples/1.9.1.8.3_M1V1.ipynb
+
 
 # Command to run jupyter nbconvert
 jupyter nbconvert --ExecutePreprocessor.allow_errors=True --to html --execute "${NOTEBOOK}" --output ~/Matthew/code/scANTIPODE/examples/outputs/"executed_$(basename "${NOTEBOOK}")" 
